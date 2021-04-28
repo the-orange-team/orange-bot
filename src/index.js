909f10bd5db397704729862f821adba451385bfd
Rômulo Gusmão <romulogmp@gmail.com>
2021-04-28 21:43:37 +0200

    chore: get no tchola

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.get("/rafaelTchola", (request, response) => {
+  return response.status(200).json({ message: "full tchola" });
+});
+app.listen(3333);
