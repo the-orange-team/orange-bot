c3ca7ecd431ed2c751f8a049db1caffbc2964c9e
Rafael Galani <galani.rafael@gmail.com>
2021-04-28 17:46:06 -0300

    feat: initial bot message implementation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import { WebClient } from '@slack/web-api';
 
+const web = new WebClient(process.env.SLACK_TOKEN);
 const app = express();
+
 app.use(express.json());
-app.get("/rafaelTchola", (request, response) => {
+app.get("/rafaelTchola", async (request, response) => {
+  await web.chat.postMessage({
+    channel: "#testes-bot",
+    text: "Romulo tchola.",
+  })
   return response.status(200).json({ message: "full tchola" });
 });
 app.listen(3333);
