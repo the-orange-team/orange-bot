19dd0109a9f2e18596e4c26c04870f76144e7515
romulogmp <romulogmp@gmail.com>
2021-05-01 09:46:49 +0200

    trying twitter package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { App } from '@slack/bolt';
 import { getValue, setValue } from './storage';
+import Twitter from 'twitter'
 
 const app = new App({
   token: process.env.SLACK_TOKEN, 
@@ -35,3 +36,22 @@ app.command('/echo', async ({ command, ack, say }) => {
     await say("MANDA O TEXTO FDP");
   }
 });
+
+
+app.message('pokedolar', async () => {
+  
+  const client = new Twitter({
+    consumer_key: process.env.TWITTER_API_KEY ?? '',
+    consumer_secret: process.env.TWITTER_API_SECRET_KEY ?? '',
+    bearer_token: process.env.TWITTER_BEARER_TOKEN ?? ''
+  });
+
+  const params = {screen_name: 'PokeDolar', count: 1};
+
+  client.get('statuses/user_timeline', params, function(error, tweets) {
+    if (!error) {
+      console.log(tweets);
+    }
+  });
+});
+
