5c242a0a66495dbd737ed229701a9830a014d58a
ThomazFB <thomazfcortez@gmail.com>
2021-05-01 22:43:31 -0300

    Remove media.ts

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,50 @@ export const app = new App({
   console.log(`⚡️ Bolt app started on port ${PORT}`);
 })();
 
+app.message('getvalue', async ({ event, context, client, say }) => {
+  try {
+    await say("getting value");
+    console.log(await getValue("teste"));
+  }
+  catch (error) {
+    app.error(error);
+  }
+});
+
+app.message(/^:.*$/, async ({ context, say }) => {
+  // RegExp matches are inside of context.matches
+  const command = context.matches[0];
+  try {
+      await say(`getting ${command.text}`);
+      const response = await getValue(command.text);
+      if(response) { await say(response.toString()) }
+      else { await say("command doesn't exist") }
+    }
+    catch (error) {
+      await say("command failed");
+      app.error(error);
+    }
+});
+
+app.command('/add', async ({ command, ack, say }) => {
+  try{
+      await ack();
+      await setValue(`:${command.text}`, command.text);
+    } catch(err){
+      await say("MANDA O TEXTO FDP");
+    }
+});
+
+app.command('/echo', async ({ command, ack, say }) => {
+  try{
+    await ack();
+    await say(`${command.text}`);
+    await setValue("test", command.text);
+  } catch(err){
+    await say("MANDA O TEXTO FDP");
+  }
+});
+
 app.message('pokedolar', async () => {
   
   const client = new Twitter({
