3688ec7a146398046a2f4cd4bc5c8cd9256f45c2
romulogmp <romulogmp@gmail.com>
2021-05-03 19:03:47 +0200

    testing payload

--- src/middlewares/pokedolar.ts
+++ src/middlewares/pokedolar.ts
@@ -1,10 +1,14 @@
 import { app } from '../app';
 import { tweetToSlackMessage } from '../messages';
 import { getLastPokeDolarTweet } from '../apis/twitter/twitter';
-app.command('/pokedolar', async ({ say }) => {
+app.command('/pokedolar', async ({ say, ack, payload }) => {
     try {
         const { tweet, mediaUrl } = await getLastPokeDolarTweet();
         await say(tweetToSlackMessage(tweet, mediaUrl));
+        await ack({
+            response_type: 'in_channel',
+            text: `Requested by :${payload.user_name}`,
+        });
     } catch (error) {
         await say('Failed to fetch last tweet.');
         app.error(error);
