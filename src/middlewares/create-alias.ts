088293ebf2f1ba3a776b6e5a2489970a9107e468
Rafael Galani <galani.rafael@gmail.com>
2021-05-03 20:56:43 -0300

    Replace HOF with middleware function

--- src/middlewares/create-alias.ts
+++ src/middlewares/create-alias.ts
@@ -3,30 +3,27 @@ import { callAuthorized } from '../middlewares/user-alias';
 import { slackCommandToCommand, createCommand } from '../messages';
 import { storage } from '../storage';
 
-app.command(
-    '/create',
-    callAuthorized(async ({ command, ack }) => {
-        try {
-            const botCommand = slackCommandToCommand(command.text);
-            if (botCommand) {
-                createCommand(botCommand, storage);
-                await ack({
-                    response_type: 'ephemeral',
-                    text: `You can now use the command writing :${botCommand.command}`,
-                });
-            } else {
-                await ack({
-                    response_type: 'ephemeral',
-                    text: 'Invalid command pattern',
-                });
-            }
-        } catch (err) {
+app.command('/create', callAuthorized, async ({ command, ack }) => {
+    try {
+        const botCommand = slackCommandToCommand(command.text);
+        if (botCommand) {
+            createCommand(botCommand, storage);
             await ack({
                 response_type: 'ephemeral',
-                text: 'Something went wrong',
+                text: `You can now use the command writing :${botCommand.command}`,
             });
-            console.error(err);
-            app.error(err);
+        } else {
+            await ack({
+                response_type: 'ephemeral',
+                text: 'Invalid command pattern',
+            });
         }
-    })
-);
+    } catch (err) {
+        await ack({
+            response_type: 'ephemeral',
+            text: 'Something went wrong',
+        });
+        console.error(err);
+        app.error(err);
+    }
+});
--- src/middlewares/user-alias.ts
+++ src/middlewares/user-alias.ts
@@ -1,10 +1,10 @@
 import { SlackCommandMiddlewareArgs, Middleware } from '@slack/bolt';
 
-export function callAuthorized(
-    listener: (...args: SlackCommandMiddlewareArgs[]) => any
-): (...args: SlackCommandMiddlewareArgs[]) => any {
-    return ({ payload, command, body, say, respond, ack }) => {
-        console.log('intercepted');
-        listener({ payload, command, body, say, respond, ack });
-    };
-}
+export const callAuthorized: Middleware<SlackCommandMiddlewareArgs> = async ({
+    next,
+    ack,
+    ...rest
+}) => {
+    console.log('intercepted');
+    await next?.();
+};
