df82df021aa5dd6a6a15dc56b6b07835c25109ed
ThomazFB <thomazfcortez@gmail.com>
2021-05-02 21:00:30 -0300

    Add console logs to list command

--- src/middlewares/list-alias.ts
+++ src/middlewares/list-alias.ts
@@ -5,11 +5,13 @@ app.command('/list', async ({ ack }) => {
     try {
         const keys = await storage.listAllValues();
         if (keys) {
+            console.log(keys);
             await ack({
                 text: keys.join(' \n '),
                 response_type: 'ephemeral',
             });
         } else {
+            console.log('empty key list');
             await ack({
                 mrkdwn: true,
                 text: `Couldn't fetch the command list`,
@@ -17,6 +19,7 @@ app.command('/list', async ({ ack }) => {
             });
         }
     } catch (err) {
+        console.log(err);
         await ack({
             mrkdwn: true,
             response_type: 'ephemeral',
