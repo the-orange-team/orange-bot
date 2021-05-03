19df220b8cd18a4ad95bf927362f8b34d51eb802
romulogmp <romulogmp@gmail.com>
2021-05-03 19:32:13 +0200

    adding log to pokedolar

--- src/middlewares/pokedolar.ts
+++ src/middlewares/pokedolar.ts
@@ -4,6 +4,7 @@ import { getLastPokeDolarTweet } from '../apis/twitter/twitter';
 
 app.command('/pokedolar', async ({ say, ack, payload }) => {
     try {
+        console.log(`[pokedolar] fetching last tweet`);
         const { tweet, mediaUrl } = await getLastPokeDolarTweet();
         await say(tweetToSlackMessage(tweet, mediaUrl, payload.user_name));
         await ack();
