952364b83ae4056f65abb9d37aa2329014f957a2
ThomazFB <thomazfcortez@gmail.com>
2021-05-04 20:38:31 -0300

    Add sentry infrastructure

---
+++ src/logger/index.ts
@@ -0,0 +1,52 @@
+import { app } from '../app';
+import { SlashCommand, Logger } from '@slack/bolt';
+import * as Sentry from '@sentry/node';
+
+const sentry = Sentry;
+
+export interface OrangeLogger {
+    logError: (error: any, payload?: SlashCommand) => void;
+    logStep: (logger: Logger, category: string, message: any, payload: SlashCommand) => void;
+}
+
+class LoggerImplementation implements OrangeLogger {
+    constructor() {
+        sentry.init({
+            dsn: process.env.SENTRY_DSN,
+        });
+    }
+
+    logError(error: any, payload?: SlashCommand) {
+        if (payload)
+            sentry.withScope((scope) => {
+                scope.setUser({
+                    user: payload.user_name,
+                    channel: payload.channel_name,
+                    command: payload.command,
+                    text: payload.text,
+                });
+                sentry.captureException(error);
+            });
+        else sentry.captureException(error);
+
+        console.error(error);
+        app.error(error);
+    }
+
+    logStep(logger: Logger, category: string, message: any, payload: SlashCommand) {
+        sentry.addBreadcrumb({
+            category: category,
+            message: message,
+            level: Sentry.Severity.Info,
+            data: {
+                user: payload.user_name,
+                channel: payload.channel_name,
+                command: payload.command,
+                text: payload.text,
+            },
+        });
+        logger.info(message);
+    }
+}
+
+export const logger = new LoggerImplementation();
