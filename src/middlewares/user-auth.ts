a8ace28bf62333d7dff6175b05efc33a0804efc4
ThomazFB <thomazfcortez@gmail.com>
2021-05-04 08:49:32 -0300

    Make devmode functional

--- src/middlewares/user-auth.ts
+++ src/middlewares/user-auth.ts
@@ -1,22 +1,25 @@
 import { app } from '../app';
 import { SlackCommandMiddlewareArgs, Middleware } from '@slack/bolt';
 
+let devModeActive = false;
+
 export const callAuthorized: Middleware<SlackCommandMiddlewareArgs> = async ({
     next,
     ack,
     ...rest
 }) => {
-    await next?.();
+    if (devModeActive == false || (devModeActive && userIsDev(rest.payload.user_id))) {
+        await next?.();
+    }
 };
 
 app.command('/devmode', async ({ payload, ack }) => {
-    const devIds = process.env.DEV_USER_GROUP;
-    const devList: string[] = devIds?.substring(1, devIds.length - 1).split(',') ?? [];
-    const devUser = devList.find((str) => str == payload.user_id);
     try {
-        if (devUser) {
+        if (userIsDev(payload.user_id)) {
+            devModeActive = !devModeActive;
+            let output: string;
             await ack({
-                text: `dev mode activated, all hail the code supremacy. Now go fix that shit and :shipit:`,
+                text: generateDevModeMessage(),
                 response_type: 'ephemeral',
             });
         } else {
@@ -32,3 +35,19 @@ app.command('/devmode', async ({ payload, ack }) => {
         });
     }
 });
+
+function userIsDev(userId: string): boolean {
+    const devIds = process.env.DEV_USER_GROUP;
+    const devList: string[] = devIds?.substring(1, devIds.length - 1).split(',') ?? [];
+    const devUser = devList.find((str) => str == userId);
+    if (devUser) return true;
+    else return false;
+}
+
+function generateDevModeMessage(): string {
+    if (devModeActive) {
+        return 'dev mode activated, all hail the code supremacy. Now go fix that shit and :shipit:';
+    } else {
+        return "I don't know who you are, I don't know what you want, but if there's any bug left, I will hunt you, and I will find you";
+    }
+}
