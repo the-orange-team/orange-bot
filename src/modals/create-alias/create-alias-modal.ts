f2895cdc0512f3441648e0563de7853c33390cdd
romulogmp <romulogmp@gmail.com>
2021-05-05 20:09:30 +0200

    Testing create-alias modal

--- src/modals/create-alias/create-alias-modal.ts
+++ src/modals/create-alias/create-alias-modal.ts
@@ -1,20 +1,66 @@
 import { SlashCommand } from '@slack/bolt';
-import axios from 'axios';
-import schema from './schema';
-
-export async function getModal(payload: SlashCommand) {
-    const config = {
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+import { ViewsOpenArguments } from '@slack/web-api';
+function getModalSchema(payload: SlashCommand): ViewsOpenArguments {
+    return {
+        trigger_id: payload.trigger_id,
+        view: {
+            title: {
+                type: 'plain_text',
+                text: 'Create your alias',
+            },
+            type: 'modal',
+            blocks: [
+                {
+                    type: 'input',
+                    element: {
+                        type: 'plain_text_input',
+                        action_id: 'title',
+                        placeholder: {
+                            type: 'plain_text',
+                            text: 'Input the command',
+                        },
+                    },
+                    label: {
+                        type: 'plain_text',
+                        text: 'Command',
+                    },
+                },
+                {
+                    type: 'input',
+                    element: {
+                        type: 'plain_text_input',
+                        action_id: 'option_1',
+                        placeholder: {
+                            type: 'plain_text',
+                            text: 'Enter the value to be returned',
+                        },
+                    },
+                    label: {
+                        type: 'plain_text',
+                        text: 'Value',
+                    },
+                },
+                {
+                    type: 'actions',
+                    elements: [
+                        {
+                            type: 'button',
+                            action_id: 'add_option',
+                            text: {
+                                type: 'plain_text',
+                                text: 'Add another option  ',
+                            },
+                        },
+                    ],
+                },
+            ],
+            callback_id: 'view_1',
+            submit: {
+                type: 'plain_text',
+                text: 'Submit',
+            },
         },
     };
-    const body = {
-        trigger_id: payload.trigger_id,
-        view: schema,
-    };
-
-    const response = axios.post('https://slack.com/api/views.open', body, config);
-    console.log(response);
-    return response;
 }
+
+export { getModalSchema };
