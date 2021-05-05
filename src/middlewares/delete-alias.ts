998a1168b2524151a67948d9d7be6891bccb4e18
ThomazFB <thomazfcortez@gmail.com>
2021-05-04 21:02:18 -0300

    Add log tracking to delete-alias

--- src/middlewares/delete-alias.ts
+++ src/middlewares/delete-alias.ts
@@ -2,31 +2,35 @@ import { app } from '../app';
 import { callAuthorized } from './user-auth';
 import { deleteCommand } from '../messages';
 import { storage } from '../storage';
+import { orangeLogger } from '../logger';
 
-app.command('/delete', callAuthorized, async ({ command, ack, logger }) => {
+const tag = 'delete-alias';
+
+app.command('/delete', callAuthorized, async ({ payload, command, ack, logger }) => {
     try {
+        orangeLogger.logStep(logger, tag, 'received', payload);
         logger.info(`[delete] deleting command invoked.`);
         const [aliasToDelete] = command.text.trim().split(' ');
 
         if (!aliasToDelete) {
+            orangeLogger.logStep(logger, tag, 'invalidated', payload);
             await ack({
                 response_type: 'ephemeral',
                 text: `Invalid command pattern.`,
             });
-        }
-        logger.info(`[delete] target alias :${aliasToDelete}. trying to delete...`);
+        } else orangeLogger.logStep(logger, tag, 'validated', payload);
+
+        orangeLogger.logStep(logger, tag, 'deleting', payload);
 
         const operationResult = await deleteCommand({ text: aliasToDelete }, storage, command);
         if (operationResult.success) {
-            logger.info(`[delete] ${aliasToDelete} deleted.`);
+            orangeLogger.logStep(logger, tag, 'deleted', payload);
             await ack({
                 response_type: 'ephemeral',
                 text: `Alias ${aliasToDelete} has been successfully deleted`,
             });
         } else {
-            logger.info(
-                `[delete] :${aliasToDelete} was not deleted. Error: ${operationResult.error}.`
-            );
+            orangeLogger.logStep(logger, tag, 'no-op', payload);
             await ack({
                 response_type: 'ephemeral',
                 text: `${operationResult.error}. No-op.`,
@@ -38,5 +42,6 @@ app.command('/delete', callAuthorized, async ({ command, ack, logger }) => {
             response_type: 'ephemeral',
             text: `Something went wrong`,
         });
+        orangeLogger.logError(err, payload);
     }
 });
