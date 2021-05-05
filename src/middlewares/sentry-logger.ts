1f286f5ef875e8c0cd9e865b7c46d68c539ad7dc
guilherme lucena <lucena9682@gmail.com>
2021-05-05 07:42:50 -0300

    remove ephemeral things from logger

--- src/middlewares/sentry-logger.ts
+++ src/middlewares/sentry-logger.ts
@@ -1,19 +1,9 @@
-import { RespondArguments, SlackCommandMiddlewareArgs } from '@slack/bolt';
+import { SlackCommandMiddlewareArgs } from '@slack/bolt';
 import { app } from '../app';
 import { orangeLogger } from '../logger';
 
 app.use(async ({ context, next, logger, ...rest }) => {
     const commandArgs = rest as Partial<SlackCommandMiddlewareArgs>;
-
-    context.sendEphemeral = (text: string) =>
-        commandArgs.ack?.({
-            mrkdwn: true,
-            response_type: 'ephemeral',
-            text,
-        });
-
-    context.sendComposedEphemeral = (composedMessage: RespondArguments) =>
-        commandArgs.ack?.(composedMessage);
 
     if (commandArgs.payload) {
         context.logStep = orangeLogger.logStep.bind(orangeLogger, logger, commandArgs.payload);
