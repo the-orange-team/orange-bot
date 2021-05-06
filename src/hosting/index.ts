14edfc6c1c96e0ab2de5d8f73448b6859f594d5e
ThomazFB <thomazfcortez@gmail.com>
2021-05-06 20:45:55 -0300

    Introduce FileSystem

--- src/hosting/index.ts
+++ src/hosting/index.ts
@@ -1,12 +1,31 @@
+import { Bucket } from '@google-cloud/storage';
 import admin from 'firebase-admin';
 
-admin.initializeApp({
-    credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    }),
-    storageBucket: process.env.FIREBASE_STORAGE_URL,
-});
+export interface FileSystem {
+    uploadURL: (url: string, fileName: string) => Promise<string>;
+}
 
-export const bucket = admin.storage().bucket();
+class FirebaseFileSystem implements FileSystem {
+    private bucket: Bucket;
+
+    constructor() {
+        admin.initializeApp({
+            credential: admin.credential.cert({
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey: process.env.FIREBASE_PRIVATE_KEY,
+            }),
+            storageBucket: process.env.FIREBASE_STORAGE_URL,
+        });
+        this.bucket = admin.storage().bucket();
+    }
+
+    uploadURL(url: string, fileName: string): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            console.log(this.bucket.name);
+            resolve('');
+        });
+    }
+}
+
+export const fileSystem = new FirebaseFileSystem();
