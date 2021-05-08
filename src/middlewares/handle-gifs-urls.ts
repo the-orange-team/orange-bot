d518aae3211e972ceefb10b08187f930af00a9ed
romulogmp <romulogmp@gmail.com>
2021-05-08 14:47:13 +0200

    Handling shortlinks

--- src/middlewares/handle-gifs-urls.ts
+++ src/middlewares/handle-gifs-urls.ts
@@ -1,29 +1,40 @@
 import { URL } from 'url';
 import { searchGiphyById } from '../apis/giphy';
-import { GIPHY_BY_NAME_ENDPOINT } from '../utils/constants';
+import { GIPHY_BY_NAME_ENDPOINT, GIPHY_SHORTLINK_ORIGIN } from '../utils/constants';
+import axios from 'axios';
 const tag = 'handle-gifs-urls';
 
 export async function urlParser(url: string): Promise<string> {
-    const functionsMap: Record<string, Promise<string> | undefined> = {
-        'https://giphy.com': giphyUrlParser(url),
-    };
     const nodeURL = new URL(url);
+    const functionsMap: Record<string, Promise<string> | undefined> = {
+        'https://giphy.com': giphyUrlParser(nodeURL),
+        'https://gph.is/': giphyShortUrlParser(nodeURL),
+    };
     const parsedUrl = (await functionsMap[nodeURL.origin]) ?? url;
     return parsedUrl;
 }
 
-async function giphyUrlParser(url: string): Promise<string> {
+async function giphyUrlParser(nodeURL: URL): Promise<string> {
     try {
+        const url = nodeURL.href;
         if (!url.includes(GIPHY_BY_NAME_ENDPOINT)) return url;
-        const nodeURL = new URL(url);
-        const paths = nodeURL.pathname.split('-');
-        const gifId = paths[paths.length - 1];
+        const pathParams = nodeURL.pathname.split('-');
+        const gifId = pathParams[pathParams.length - 1];
         if (!gifId) return url;
         const giphyUrl = await searchGiphyById(gifId);
         if (!giphyUrl) return url;
         return giphyUrl;
     } catch (err) {
         console.error(err);
-        return url;
+        return nodeURL.href;
     }
+}
+
+async function giphyShortUrlParser(nodeURL: URL): Promise<string> {
+    const url = nodeURL.href;
+    if (!url.includes(GIPHY_SHORTLINK_ORIGIN)) return url;
+    const response = await axios.get(url);
+    console.log(response);
+
+    return '';
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
 export const GIPHY_BY_NAME_ENDPOINT = 'https://giphy.com/gifs/';
+export const GIPHY_SHORTLINK_ORIGIN = 'https://gph.is/';
 export const GIPHY_ORIGIN = 'https://giphy.com';
