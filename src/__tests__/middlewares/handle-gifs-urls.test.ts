1829a8843df334a925c21061694c978954263277
romulogmp <romulogmp@gmail.com>
2021-05-08 21:53:20 +0200

    Add urlParser tests

---
+++ src/__tests__/middlewares/handle-gifs-urls.test.ts
@@ -0,0 +1,25 @@
+import { urlParser } from '../../middlewares/handle-gifs-urls';
+import axios from 'axios';
+import { mocked } from 'ts-jest/utils';
+
+jest.mock('axios');
+const axiosMock = mocked(axios.get, true);
+
+describe('handle-gifs-urls', () => {
+    test('should return formated url for short links', async () => {
+        axiosMock.mockResolvedValueOnce({
+            request: {
+                path: '9PIh0i0Qs3fiNL7PKf',
+            },
+            status: 200,
+            statusText: 'ok',
+            config: {},
+        });
+        const url = await urlParser('https://gph.is/g/EG79Mbn');
+        expect(url).toEqual('https://media.giphy.com/media/9PIh0i0Qs3fiNL7PKf/giphy.gif');
+    });
+    test('should return formated url for giphy.com gifs', async () => {
+        const url = await urlParser('https://giphy.com/gifs/horse-applause-clapping-8RxCFgu88jUbe');
+        expect(url).toEqual('https://media.giphy.com/media/8RxCFgu88jUbe/giphy.gif');
+    });
+});
