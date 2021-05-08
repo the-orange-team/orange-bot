dcddbbd1b8c918b19634da3dc09b48b19bc0df00
romulogmp <romulogmp@gmail.com>
2021-05-08 20:21:11 +0200

    Refactoring urlParser

--- src/middlewares/handle-gifs-urls.ts
+++ src/middlewares/handle-gifs-urls.ts
@@ -4,46 +4,43 @@ import axios from 'axios';
 
 export async function urlParser(url: string): Promise<string> {
     const nodeURL = new URL(url);
-    const functionsMap: Record<string, Promise<string> | string | undefined> = {
+    const functionsMap: Record<string, Promise<string | undefined> | string | undefined> = {
         'https://giphy.com': giphyUrlParser(nodeURL),
         'http://giphy.com': giphyUrlParser(nodeURL),
         'https://gph.is': giphyShortUrlParser(nodeURL),
         'http://gph.is': giphyShortUrlParser(nodeURL),
     };
-    const parsedUrl = (await functionsMap[nodeURL.origin]) ?? url;
+    const parsedUrl = await functionsMap[nodeURL.origin];
     if (!parsedUrl) return url;
     return parsedUrl;
 }
 
-function giphyUrlParser(nodeURL: URL): string {
+function giphyUrlParser(nodeURL: URL): string | undefined {
     try {
         const url = nodeURL.href;
         if (!url.includes(GIPHY_BY_NAME_ENDPOINT)) return url;
-        const pathParams = nodeURL.pathname.split('-');
-        const gifId = pathParams[pathParams.length - 1];
-        if (!gifId) return url;
-        const giphyUrl = `https://media.giphy.com/media/${gifId}/giphy.gif`;
-        if (!giphyUrl) return url;
-        return giphyUrl;
+        const gifId = getGiphyId(nodeURL.pathname);
+        if (gifId) return `https://media.giphy.com/media/${gifId}/giphy.gif`;
     } catch (err) {
         console.error(err);
         return nodeURL.href;
     }
 }
-async function giphyShortUrlParser(nodeURL: URL): Promise<string> {
+
+async function giphyShortUrlParser(nodeURL: URL): Promise<string | undefined> {
     try {
         const url = nodeURL.href;
         if (!url.includes(GIPHY_SHORTLINK_ORIGIN)) return url;
-        const config = { maxRedirects: 1 };
-        const response = await axios.get(url, config);
-        const pathParams = response.request.path.split('-');
-        const gifId = pathParams[pathParams.length - 1];
-        if (!gifId) return url;
-        const giphyUrl = `https://media.giphy.com/media/${gifId}/giphy.gif`;
-        if (!giphyUrl) return url;
-        return giphyUrl;
+        const response = await axios.get(url);
+        const gifId = getGiphyId(await response.request.path);
+        if (gifId) return `https://media.giphy.com/media/${gifId}/giphy.gif`;
     } catch (err) {
         console.error(err);
         return nodeURL.href;
     }
+}
+
+function getGiphyId(pathParams: string) {
+    const params = pathParams.split('-');
+    return params[params.length - 1];
 }
