a1b070fd46ead0b4badc30037bdda99a0ed2ec6d
romulogmp <romulogmp@gmail.com>
2021-05-08 21:53:54 +0200

    Add constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,8 @@
 export const GIPHY_BY_NAME_ENDPOINT = '//giphy.com/gifs/';
 export const GIPHY_SHORTLINK_ORIGIN = '//gph.is/';
 export const GIPHY_ORIGIN = 'https://giphy.com';
+
+export const GIPHY_HTTPS = 'https://giphy.com';
+export const GIPHY_HTTP = 'http://giphy.com';
+export const GIPHY_SHORLINK_HTTPS = 'https://gph.is';
+export const GIPHY_SHORTLINK_HTTP = 'http://gph.is';
