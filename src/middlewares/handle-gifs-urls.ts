139d1bf108b79521ea75426f9bc2c778883d8f5f
romulogmp <romulogmp@gmail.com>
2021-05-08 11:49:20 +0200

    Add request by giphy Id

--- src/middlewares/handle-gifs-urls.ts
+++ src/middlewares/handle-gifs-urls.ts
@@ -1,26 +1,24 @@
 import { URL } from 'url';
 import { searchGiphyById } from '../apis/giphy';
-
+import { GIF_DOMAIN } from '../utils/constants';
 const tag = 'handle-gifs-urls';
 
 export async function giphyUrlParser(url: string): Promise<string> {
+    if (!url.includes(GIF_DOMAIN)) return url;
+
     const nodeURL = new URL(url);
-    const paths = nodeURL.pathname.split('/');
+    const paths = nodeURL.pathname.split('-');
     console.log(`path params ${paths}`);
-    let giphyUrl = url;
+
     console.log('checking path params');
-    paths.forEach(async (path) => {
-        if (
-            !path.includes('giphy.gif') &&
-            !path.includes('media') &&
-            !path.includes('media0') &&
-            path
-        ) {
-            const gifId = path;
-            console.log(`found ${gifId}`);
-            giphyUrl = await searchGiphyById(gifId);
-            console.log(`returning ${giphyUrl}`);
-        }
-    });
+    const gifId = paths[paths.length - 1];
+    console.log(`found ${gifId}`);
+    if (!gifId) return url;
+
+    const giphyUrl = await searchGiphyById(gifId);
+    console.log(`returning ${giphyUrl}`);
+
+    if (!gifId && !giphyUrl) return url;
+
     return giphyUrl;
 }
