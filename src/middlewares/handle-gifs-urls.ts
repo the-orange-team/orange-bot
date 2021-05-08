942170289fb614117a9134edb632cfaf031e9ced
romulogmp <romulogmp@gmail.com>
2021-05-08 16:33:56 +0200

    Trying black magic

--- src/middlewares/handle-gifs-urls.ts
+++ src/middlewares/handle-gifs-urls.ts
@@ -6,33 +6,33 @@ const tag = 'handle-gifs-urls';
 
 export async function urlParser(url: string): Promise<string> {
     const nodeURL = new URL(url);
-    const functionsMap: Record<string, Promise<string> | undefined> = {
-        'https://giphy.com': giphyUrlParser(nodeURL),
-        'https://gph.is/': giphyShortUrlParser(nodeURL),
+    const functionsMap: Record<string, (nodeURL: URL) => Promise<string> | string | undefined> = {
+        'https://giphy.com': makeGiphyUrlParser,
+        'https://gph.is/': makeGiphyShortUrlParser,
     };
-    const parsedUrl = await functionsMap[nodeURL.origin];
+    const urlAdpter = await functionsMap[nodeURL.origin];
+    const parsedUrl = await urlAdpter(nodeURL);
     console.log(`giphyUrl ${parsedUrl}`);
     if (!parsedUrl) return url;
     return parsedUrl;
 }
 
-async function giphyUrlParser(nodeURL: URL): Promise<string> {
+const makeGiphyUrlParser = (nodeURL: URL): string => {
     try {
         const url = nodeURL.href;
         if (!url.includes(GIPHY_BY_NAME_ENDPOINT)) return url;
         const pathParams = nodeURL.pathname.split('-');
         const gifId = pathParams[pathParams.length - 1];
         if (!gifId) return url;
-        const giphyUrl = await searchGiphyById(gifId);
+        const giphyUrl = `https://media.giphy.com/media/${gifId}/giphy.gif`;
         if (!giphyUrl) return url;
         return giphyUrl;
     } catch (err) {
         console.error(err);
         return nodeURL.href;
     }
-}
-
-async function giphyShortUrlParser(nodeURL: URL): Promise<string> {
+};
+const makeGiphyShortUrlParser = async (nodeURL: URL): Promise<string> => {
     try {
         const url = nodeURL.href;
         console.log(`nodeURL.href ${url}`);
@@ -52,4 +52,4 @@ async function giphyShortUrlParser(nodeURL: URL): Promise<string> {
         console.error(err);
         return nodeURL.href;
     }
-}
+};
