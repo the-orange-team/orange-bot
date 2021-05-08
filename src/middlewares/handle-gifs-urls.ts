7386d6fb670a3bb2d892df64e40c529115a47d4f
romulogmp <romulogmp@gmail.com>
2021-05-08 13:07:14 +0200

    Removing console.logs

--- src/middlewares/handle-gifs-urls.ts
+++ src/middlewares/handle-gifs-urls.ts
@@ -7,29 +7,23 @@ export async function urlParser(url: string): Promise<string> {
     const functionsMap: Record<string, Promise<string> | undefined> = {
         'https://giphy.com': giphyUrlParser(url),
     };
-
     const nodeURL = new URL(url);
     const parsedUrl = (await functionsMap[nodeURL.origin]) ?? url;
-
     return parsedUrl;
 }
 
 async function giphyUrlParser(url: string): Promise<string> {
-    if (!url.includes(GIPHY_BY_NAME_ENDPOINT)) return url;
-
-    const nodeURL = new URL(url);
-    const paths = nodeURL.pathname.split('-');
-    console.log(`path params ${paths}`);
-
-    console.log('checking path params');
-    const gifId = paths[paths.length - 1];
-    console.log(`found ${gifId}`);
-    if (!gifId) return url;
-
-    const giphyUrl = await searchGiphyById(gifId);
-    console.log(`returning ${giphyUrl}`);
-
-    if (!gifId && !giphyUrl) return url;
-
-    return giphyUrl;
+    try {
+        if (!url.includes(GIPHY_BY_NAME_ENDPOINT)) return url;
+        const nodeURL = new URL(url);
+        const paths = nodeURL.pathname.split('-');
+        const gifId = paths[paths.length - 1];
+        if (!gifId) return url;
+        const giphyUrl = await searchGiphyById(gifId);
+        if (!giphyUrl) return url;
+        return giphyUrl;
+    } catch (err) {
+        console.error(err);
+        return url;
+    }
 }
