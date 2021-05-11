27f7512aab4ea0190ffa54fb4ee4d27eb9e7e437
ThomazFB <thomazfcortez@gmail.com>
2021-05-11 03:53:18 -0300

    Update help text

--- src/middlewares/user-help.ts
+++ src/middlewares/user-help.ts
@@ -6,60 +6,79 @@ app.command('/help', async ({ context }) => {
     context.logStep(tag, 'received');
     await context.sendComposedEphemeral({
         text: 'You asked for help?',
-        blocks: [
-            {
-                type: 'section',
-                text: {
-                    type: 'mrkdwn',
-                    text:
-                        'COÉ RAPAZIADAAAAA :jenkins_triggered: :ugo-triggered: :thomaz-triggered:  Eu sou o Orangebot :tangerine:, e isso é um guia sobre como usar meus comandos corretamente.',
-                },
-            },
-            {
-                type: 'divider',
-            },
-            {
-                type: 'section',
-                text: {
-                    type: 'mrkdwn',
-                    text:
-                        '*:one: Use o `/create` para criar um alias*. Escreva `/create` seguido do seguinte padrão `[alias] returning [url]`. Exemplo: `/create suckithaters returning http://i.imgur.com/4yuup.gif` \n\n\n :warning::warning::warning: SE LIGUE NO SEGUINTE, O NOME DO ALIAS NÃO PODE COMEÇAR COM O CARACTERE `:` EIM. \n\n se você tentar `/create :suckithaters returning http://i.imgur.com/4yuup.gif`, isso não vai funcionar :porra:',
-                },
-            },
-            {
-                type: 'divider',
-            },
-            {
-                type: 'section',
-                text: {
-                    type: 'mrkdwn',
-                    text:
-                        '*:two: Convocando um alias*. Dado que um alias existe, apenas escreva `:[alias]` que eu vou postar o que foi salvo como valor para esse alias. Ou seja escrevendo `:suckithaters` irá causar isso:',
-                },
-            },
-            {
-                type: 'image',
-                title: {
-                    type: 'plain_text',
-                    text: 'image1',
-                    emoji: true,
-                },
-                image_url: 'https://i.ibb.co/BB0csvw/Screen-Shot-2021-05-04-at-16-24-34.png',
-                alt_text: 'image1',
-            },
-            {
-                type: 'divider',
-            },
-            {
-                type: 'context',
-                elements: [
-                    {
+        blocks: {
+            blocks: [
+                {
+                    type: 'section',
+                    text: {
                         type: 'mrkdwn',
-                        text:
-                            'Esse bot foi feito com o puro suco do ódio por :guribadass:,:brabo:,:romulold: e :smoked-thomaz:, usem o `@ orangebotdevs` se precisarem de ajuda \n\n\n つづく',
+                        text: 'Olar, eu sou o Orangebot :tangerine:, e já que você chamou o help sem passar nenhum argumento, eu vou dar uma descrição geral de como eu funciono,e aqui vai também um guia de como utilizar o help e explorar as minhas funcionalidades.',
                     },
-                ],
-            },
-        ],
+                },
+                {
+                    type: 'divider',
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: ':warning: Primeiramente, é importante entender que aqui no nosso Slack sempre foi utilizado o caractere `:` como forma de invocar um texto ou link armazenado diretamente no slackbot, por uma facilidade de referência, eu chamo esses textos que invocam um comportamento de `alias`. \n\n Ou seja, quando você digita `:qualquercoisa`, eu vou receber o alias `qualquercoisa` e buscar no meu banco de dados se há alguma resposta em link/imagem/texto para isso. Se o link for de imagem ou GIF, eu irei enviar ele no formato de bloco de imagem dedicado, sem necessidade de chamar thumbnails. Dito isso, aqui vai a lista de guias disponíveis.',
+                    },
+                },
+                {
+                    type: 'divider',
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: '- `/help create` para ver as instruções de como criar um `alias` pela UI do slack',
+                    },
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: '- `/help cmdcrt` para ver as instruções de como criar um `alias` por comando de texto',
+                    },
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: '- `/help delete` para ver as instruções de como deletar um `alias`',
+                    },
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: '- `/help replace` para ver as instruções de como substituir um `alias`',
+                    },
+                },
+                {
+                    type: 'divider',
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: 'Vocês podem acabar notando que eu tenho alguns comandos não descritos acima que aparecem listados na barra de texto, são eles: `/list`, `/pokerdolar`, `/reset` e o `/devmode`. O `/list` e `/pokerdolar` simplesmente pela razão de que você deve ser retardado se precisa de ajuda pra usar um comando que leva zero argumentos, apenas use e entenda. \n\n Já o `/reset` destroi completamente o meu banco de dados, logo o comando exige uma senha que apenas meus desenvolvedores tem, o mesmo vale para o `/devmode`, que desabilita que eu responda qualquer comando complexo caso algum problema grave apareça e seja necessário parar minha utilização.',
+                    },
+                },
+                {
+                    type: 'divider',
+                },
+                {
+                    type: 'context',
+                    elements: [
+                        {
+                            type: 'mrkdwn',
+                            text: 'O orangebot foi feito por Guri :guribadass:, Rafael :brabo:, Rômulo :romulold: e Thomaz :smoked-thomaz:. Se der merda reclamem com eles. Ou resolva por conta própria aqui: https://github.com/the-orange-team/orange-bot',
+                        },
+                    ],
+                },
+            ],
+        },
     });
 });
