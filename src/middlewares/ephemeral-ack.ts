82a2a7eb4327a999a8cbc5d79b75272776c1650d
Rafael Galani <galani.rafael@gmail.com>
2021-05-10 23:03:08 -0300

    Add ack for action listeners

--- src/middlewares/ephemeral-ack.ts
+++ src/middlewares/ephemeral-ack.ts
@@ -1,6 +1,8 @@
 import {
     AllMiddlewareArgs,
+    onlyViewActions,
     RespondArguments,
+    SlackActionMiddlewareArgs,
     SlackCommandMiddlewareArgs,
     SlackViewMiddlewareArgs,
 } from '@slack/bolt';
@@ -29,11 +31,26 @@ app.use(async (args) => {
 app.use(async (args) => {
     //TODO: replace the if statement with the following commented code.
     // https://github.com/slackapi/bolt-js/issues/911
-    //onlyActions(args);
+    //onlyViewActions(args);
 
     //TODO: replace the code with the appropriate `ack` arguments when using views.
     const { context, ack, view } = args as SlackViewMiddlewareArgs & AllMiddlewareArgs;
     if (!view) return await args.next?.();
+
+    context.sendEphemeral = (text: string) => ack();
+    context.sendComposedEphemeral = (args: RespondArguments) => ack();
+
+    return await args.next?.();
+});
+
+app.use(async (args) => {
+    //TODO: replace the if statement with the following commented code.
+    // https://github.com/slackapi/bolt-js/issues/911
+    //onlyActions(args);
+
+    //TODO: replace the code with the appropriate `ack` arguments when using views.
+    const { context, ack, action } = args as SlackActionMiddlewareArgs & AllMiddlewareArgs;
+    if (!action) return await args.next?.();
 
     context.sendEphemeral = (text: string) => ack();
     context.sendComposedEphemeral = (args: RespondArguments) => ack();
