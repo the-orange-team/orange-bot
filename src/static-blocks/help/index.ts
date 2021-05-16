4efd5bb38d3113d75db5f8bb632d0786ac20269c
ThomazFB <thomazfcortez@gmail.com>
2021-05-16 10:14:27 -0300

    Add help blocks for multiple commands

--- src/static-blocks/help/index.ts
+++ src/static-blocks/help/index.ts
@@ -1,8 +1,20 @@
 import { RespondArguments } from '@slack/bolt';
 import main from './help-main.json';
+import create from './help-create.json';
+import cmdcrt from './help-cmdcrt.json';
+import hidden from './help-hidden.json';
+import deletion from './help-deletion.json';
 
 export function generateHelpBlock(argument: string): RespondArguments {
     switch (argument) {
+        case 'create':
+            return create;
+        case 'cmdcrt':
+            return cmdcrt;
+        case 'delete':
+            return deletion;
+        case 'hidden':
+            return hidden;
         default:
             return main;
     }
