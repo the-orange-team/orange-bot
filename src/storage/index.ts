30b0905ee39befce905f5ced338f6b37c99833e0
GuiLucena <lucena9682@gmail.com>
2022-07-27 14:04:29 -0300

    Change reddis client creation

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -2,12 +2,11 @@ import redis, { ClientOpts } from 'redis';
 import { StorageImplementation } from './redis-storage';
 export * from './types';
 
-const PORT = Number(process.env.REDIS_PORT) || 3333;
 const redisUrl = process.env.REDIS_URL || '';
-const configClient: ClientOpts = {
-    auth_pass: process.env.REDIS_PASSWORD,
+const configClient: ClientOpts = {    
+    url: redisUrl
 };
 
-const redisClient = redis.createClient(PORT, redisUrl, configClient);
+const redisClient = redis.createClient(configClient);
 
 export const storage = new StorageImplementation(redisClient);
