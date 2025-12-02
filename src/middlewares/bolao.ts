516ce29340ab1c40a13154f8e48c2201c265ad4e
romulogmp <romulogmp@gmail.com>
2025-12-02 17:40:37 +0100

    chore: removendo lixo

--- src/middlewares/bolao.ts
+++
@@ -1,5 +0,0 @@
-import { app } from '../app';
-import { bolaoCommandHandler, handleBolaoPredictModal } from '../bolao/commands';
-
-app.command('/bolao', bolaoCommandHandler);
-app.view('bolao_predict_modal', handleBolaoPredictModal);
